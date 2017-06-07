b2b9474b988da28ea0b7f762b88f6677a6611372
Raval <parth.raval@capgemini.com>
2017-06-07 16:35:46 +0530

    Initial Commit

---
+++ Application.java
@@ -0,0 +1,12 @@
+public class Application{
+	 
+   /**
+    *
+    * @param args
+    * @throws Exception
+    */
+   public static void main(String[] args) throws Exception {
+       System.out.println("Hello World");
+   }
+
+}
